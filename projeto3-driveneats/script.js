f734240bdacb393ce0eb402235ff2d2ab91d944b
Cherrying55 <cherrying556@protonmail.com>
2022-08-12 20:41:48 -0300

    Seletor e modificação de botão

--- projeto3-driveneats/script.js
+++ projeto3-driveneats/script.js
@@ -1,9 +1,65 @@
-const opçoes = document.querySelectorAll(".opçao");
-for(const i of opçoes){
-    i.addEventListener("click", greenborder);
-};
+function selecionarsobremesa(num){
 
-function greenborder(e){
-    if(e.target.)
-    e.target.setAttribute("style", "border: 3px solid green;");
-};
+    let selecionado = document.querySelector('.sobremesa .green');
+
+    if ( selecionado !== null){
+        selecionado.classList.remove('green');
+    }
+
+    const seletor = '.'+num;    
+    const div = document.querySelector(seletor);
+    div.classList.add('green');
+    selecionado = document.querySelector('.comida .green');
+    const button = document.querySelector('.donebutton');
+    const selecionado2 = document.querySelector('.bebida .green');
+    const selecionado3 = document.querySelector('.comida .green');
+    if(selecionado !== null && selecionado2 !== null && selecionado3 !== null){
+        button.textContent = "Concluir pedido";
+        button.style.cssText = "background-color: green;";
+    }
+
+}
+function selecionarbebida(num){
+
+    let selecionado = document.querySelector('.bebida .green');
+
+    if ( selecionado !== null){
+        selecionado.classList.remove('green');
+    }
+
+    const seletor = '.'+num;    
+    const div = document.querySelector(seletor);
+    div.classList.add('green');
+    selecionado = document.querySelector('.comida .green');
+    const button = document.querySelector('.donebutton');
+    const selecionado2 = document.querySelector('.sobremesa .green');
+    const selecionado3 = document.querySelector('.comida .green');
+    if(selecionado !== null && selecionado2 !== null && selecionado3 !== null){
+        button.textContent = "Concluir pedido";
+        button.style.cssText = "background-color: green;";
+    }
+
+}
+function selecionarcomida(num){
+
+    let selecionado = document.querySelector('.comida .green');
+
+    if ( selecionado !== null){
+        selecionado.classList.remove('green');
+    }
+
+    const seletor = '.'+num;    
+    const div = document.querySelector(seletor);
+    div.classList.add('green');
+    selecionado = document.querySelector('.comida .green');
+
+    const button = document.querySelector('.donebutton');
+    const selecionado2 = document.querySelector('.sobremesa .green');
+    const selecionado3 = document.querySelector('.bebida .green');
+    
+    if(selecionado !== null && selecionado2 !== null && selecionado3 !== null){
+        button.textContent = "Concluir pedido";
+        button.style.cssText = "background-color: green;";
+    }
+
+}
---
+++ projeto3-driveneats/script2.js
@@ -0,0 +1,76 @@
+let arrcomida = [];
+let arrbebida = [];
+let arrsobremesa = [];
+let selecionados3 = false;
+const opçoescomida = document.querySelectorAll(".comida");
+const opçoesbebida = document.querySelectorAll(".bebida");
+const opçoessobremesa = document.querySelectorAll(".sobremesa");
+
+const button = document.querySelector(".donebutton");
+for(const i of opçoescomida){
+    i.addEventListener('click', seleçaocomida, false);
+};
+for(const i of opçoesbebida){
+    i.addEventListener('click', seleçaobebida, false);
+};
+for(const i of opçoessobremesa){
+    i.addEventListener('click', seleçaosobremesa, false);
+};
+
+/* Not e.target but the parent selected */
+function seleçaocomida(e){
+
+    e.stopPropagation();
+    if(arrcomida.length === 0){
+    arrcomida.push(this);
+    arrcomida[0].classList.toggle("green");
+    }
+    else{
+    arrcomida[0].classList.toggle("green");
+    arrcomida.pop();
+    arrcomida.push(this);
+    this.classList.toggle("green");
+    };
+    if(arrbebida.length > 0 && arrcomida.length > 0 && arrsobremesa.length > 0){
+        button.textContent = "Concluir pedido";
+        button.style.cssText = "background-color: green;";
+    }
+}
+function seleçaosobremesa(e){
+
+    e.stopPropagation();
+    if(arrsobremesa.length === 0){
+    arrsobremesa.push(this);
+    arrsobremesa[0].classList.toggle("green");
+    }
+    else{
+    arrsobremesa[0].classList.toggle("green");
+    arrsobremesa.pop();
+    arrsobremesa.push(this);
+    this.classList.toggle("green");
+    };
+    if(arrbebida.length > 0 && arrcomida.length > 0 && arrsobremesa.length > 0){
+        button.textContent = "Concluir pedido";
+        button.style.cssText = "background-color: green;";
+    }
+}
+function seleçaobebida(e){
+
+    if(arrbebida.length === 0){
+    arrbebida.push(this);
+    arrbebida[0].classList.toggle("green");
+    }
+    else{
+    arrbebida[0].classList.toggle("green");
+    arrbebida.pop();
+    arrbebida.push(this);
+    this.classList.toggle("green");
+    };
+
+    if(arrbebida.length > 0 && arrcomida.length > 0 && arrsobremesa.length > 0){
+        button.textContent = "Concluir pedido";
+        button.style.cssText = "background-color: green;";
+    }
+}
+
+
